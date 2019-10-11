7b323d68c45add8ff7f429fbe61c01138f49771d
boyan <yangbinyhbn@gmail.com>
2019-10-11 13:50:58 +0800

    init

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:result/result.dart' as result;
+
+main(List<String> arguments) {
+  print('Hello world: ${result.calculate()}!');
+}
---
+++ test/result_test.dart
@@ -0,0 +1,8 @@
+import 'package:result/result.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
